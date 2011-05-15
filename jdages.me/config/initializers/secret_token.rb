f63514587186226d2f553275dc360b756bbec9ae
John C. Dages <John.Dages@collaborativeit.com>
2011-05-15 11:19:56 -0400

    Initial rails commit

---
+++ jdages.me/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JdagesMe::Application.config.secret_token = '64779fbfae299985267205db23ef09841557155fbfff89eafd487f672df7effa8b9e995f51fdb81063603f5a83302a21f572dcca08741f886fd63ab4373f48f7'
